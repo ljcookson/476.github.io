59b9f9dec34d5b3e781b44464286dc573b5f1f0e
ljcookson <ljc806@uregina.ca>
2023-01-23 02:45:38 -0600

    LC-Jan22-2

---
+++ Scripts/primary.js
@@ -0,0 +1,11 @@
+//  This JavaScript file is needed for all pages
+
+//  Current Date Configuration
+function currentDate(){
+    var months = ["January", "February","March","April","May","June", "July", "August", "September","October", "November", "December"];
+    var dt = new Date();
+    var year = dt.getFullYear();
+    var month = months[dt.getMonth()];
+    var day = dt.getDate();
+    document.getElementById('date').innerHTML= month + " " + day + ", " + year;
+}
---
+++ Scripts/registrations.js
@@ -0,0 +1,15 @@
+//  Add New Year Registrations
+
+//  Edit Year Registrations
+
+//  Confirmation Registrations
+
+//  Delete Year Registrations
+
+//  Login Registrations
+
+//  Maintenance Registrations
+
+//  Monthly View Registrations
+
+//  Semester View Registrations
